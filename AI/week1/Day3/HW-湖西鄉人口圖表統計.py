f0bc467d9014d878f75155220a860220bdebbce3
Roy Zeng <boyud9.5@gmail.com>
2026-06-30 22:04:04 +0800

    Auto-commit: nightly check 2026-06-30

---
+++ AI/week1/Day3/HW-湖西鄉人口圖表統計.py
@@ -0,0 +1,8 @@
+import pandas as pd
+path = r'C:\Users\Roy\Documents\GitHub\python\AI\week1\Day3\52de6007-a837-4827'
+
+b = a.split("\n")
+
+c = []
+for i in range(len(b)):
+    c.append(str(b[i]).split(","))
